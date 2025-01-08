899e0db158960000d4ab8a090479383cc3c768dd
pandyamarut <pandyamarut@gmail.com>
2025-01-08 01:18:15 -0800

    create config code
    Signed-off-by: pandyamarut <pandyamarut@gmail.com>

--- src/handler.py
+++ src/handler.py
@@ -3,6 +3,7 @@ import os
 from train import train
 from utils import make_valid_config, get_output_dir, validate_env, set_config_env_vars
 from huggingface_hub._login import login
+import yaml
 
 BASE_VOLUME = os.environ.get("BASE_VOLUME", "/runpod-volume")
 if not os.path.exists(BASE_VOLUME):
@@ -17,22 +18,23 @@ async def handler(job):
     run_id = inputs["run_id"]
     args = inputs["args"]
     
-    set_config_env_vars(args)
+    # set_config_env_vars(args)
     
     # Set output directory
     output_dir = os.path.join(BASE_VOLUME, get_output_dir(run_id))
     args["output_dir"] = output_dir
     
     # First save args to a temporary config file
-    config_path = "/workspace/llm-finetuning/src/config/config.yaml"
+    config_path = "/workspace/test_config.yaml"
+    
         
     # Add run_name and job_id to args before saving
     args["run_name"] = run_id
     args["runpod_job_id"] = runpod_job_id
-    
-    # Save the config
-    # with open(config_path, "w") as f:
-    #     yaml.dump(args, f)
+
+    yaml_data = yaml.dump(args, default_flow_style=False)
+    with open(config_path, "w") as file:
+        file.write(yaml_data)
     
     # Handle credentials
     credentials = inputs["credentials"]
@@ -50,6 +52,8 @@ async def handler(job):
     # Cleanup
     del os.environ["WANDB_API_KEY"]
     del os.environ["HF_TOKEN"]
+
+
 
 runpod.serverless.start({
     "handler": handler,
